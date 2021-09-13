4a38d9f6cbdb606944830301f772cca2af162546
Kevin Huber <kevin.huber@hotmail.com>
2021-09-13 18:56:18 +0200

    Add test for DefaultActiveMqConnectionFactory

---
+++ src/test/java/org/kryonite/service/DefaultActiveMqConnectionFactoryTest.java
@@ -0,0 +1,33 @@
+package org.kryonite.service;
+
+import com.rabbitmq.client.Address;
+import com.rabbitmq.client.ConnectionFactory;
+import org.junit.jupiter.api.Test;
+import org.kryonite.api.ActiveMqConnectionFactory;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DefaultActiveMqConnectionFactoryTest {
+
+  @Test
+  void shouldSetupConnectionFactory() {
+    String username = "testee";
+    String password = "testee";
+
+    // Arrange
+    ActiveMqConnectionFactory testee = new DefaultActiveMqConnectionFactory(
+        List.of(new Address("localhost", 5672)),
+        username,
+        password
+    );
+
+    // Act
+    ConnectionFactory connectionFactory = testee.getConnectionFactory();
+
+    // Assert
+    assertEquals(username, connectionFactory.getUsername());
+    assertEquals(password, connectionFactory.getPassword());
+  }
+}
