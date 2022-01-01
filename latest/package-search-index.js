ea9e4f2ef24d81e97c8a6eb32e74ed40e9457dcf
Kryonite Docs <docs@kryonite.org>
2022-01-01 21:04:28 +0000

    Deploying to docs from @ kryonitelabs/kryo-messaging@d0fdaa1f3c4396e8e8bc9af5b9b58aa1dd59ffa5 🚀

--- latest/package-search-index.js
+++ latest/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.kryonite.kryomessaging.api"},{"l":"org.kryonite.kryomessaging.service"},{"l":"org.kryonite.kryomessaging.service.message"},{"l":"org.kryonite.kryomessaging.util"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.kryonite.kryomessaging.api"},{"l":"org.kryonite.kryomessaging.service"},{"l":"org.kryonite.kryomessaging.service.message"},{"l":"org.kryonite.kryomessaging.util"}];updateSearchResults();
